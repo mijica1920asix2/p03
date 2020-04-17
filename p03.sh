86a0c46864c5b44fbecf73872e9cd6d5e21f55df
Miguel Jimenez Cassotti <miguel.jimenez.sds@gmail.com>
2020-04-17 17:29:35 +0200

    p03.sh

---
+++ p03.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+clear
+#######COMPROVANT SI L'ARXIU DE GUIÓ L'EXECUTA L'USUARI ROOT###################
+if (( EUID != 0 ))
+then
+  echo "Aquest script s'ha d'executar amb prilegis de l'usuari root"
+  exit 9 # He posat aquest número per assegurar-me que no coincideix amb cap altre valor de retorn
+fi
+
